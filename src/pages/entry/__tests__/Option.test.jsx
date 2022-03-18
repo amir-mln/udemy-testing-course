4635050a6bf8c853a4aa4a709e4275396b21be28
amir-mln <devcareer.me@gmail.com>
2022-03-18 16:31:13 +0330

    added topping components and tests

--- src/pages/entry/__tests__/Option.test.jsx
+++ src/pages/entry/__tests__/Option.test.jsx
@@ -11,3 +11,17 @@ test("displays images for each scoop option from server", async () => {
   expect(scoopImages).toHaveLength(2);
   expect(imagesAlts).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+
+test("displays images for each topping options from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  const images = await screen.findAllByRole("img", { name: /topping$/i });
+  const imagesAlts = images.map((img) => img.alt);
+
+  expect(images).toHaveLength(3);
+  expect(imagesAlts).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
+});
