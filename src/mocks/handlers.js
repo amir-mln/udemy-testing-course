7887a4cf644b963adf80692ab4757a0d3812ebcc
amir-mln <devcareer.me@gmail.com>
2022-03-18 16:30:59 +0330

    typo

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,7 +1,7 @@
 import { rest } from "msw";
 
 export const handlers = [
-  rest.get("https://localhost:3030/scoops", (req, res, ctx) => {
+  rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
     return res(
       ctx.json([
         { name: "Chocolate", imagePath: "/images/chocolate.png" },
@@ -9,4 +9,13 @@ export const handlers = [
       ])
     );
   }),
+  rest.get("http://localhost:3030/toppings", (_, res, ctx) =>
+    res(
+      ctx.json([
+        { name: "Cherries", imagePath: "/images/cherries.png" },
+        { name: "M&Ms", imagePath: "/images/m-and-ms.png" },
+        { name: "Hot fudge", imagePath: "/images/hot-fudge.png" },
+      ])
+    )
+  ),
 ];
