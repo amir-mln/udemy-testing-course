4b8d7c903525d6924eb2c57887c2e9d3bde472c6
amir-mln <devcareer.me@gmail.com>
2022-03-19 12:15:01 +0330

    finished section `5. simulating server...`

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,18 +1,20 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
+import AlertBanner from "../common/AlertBanner";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   // optionType is 'scoops' or 'toppings'
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch(console.log);
+      .catch(() => setError(true));
   }, [optionType]);
 
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
@@ -24,6 +26,8 @@ export default function Options({ optionType }) {
       imagePath={item.imagePath}
     />
   ));
+
+  if (error) return <AlertBanner />;
 
   return <Row>{optionItems}</Row>;
 }
